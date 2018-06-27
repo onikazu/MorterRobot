ef0bfbf695b322817619e50ac4dc9cdc73731df3
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-6.local>
2018-06-27 14:44:52 +0900

    revise robot

--- threading_robot.py
+++ threading_robot.py
@@ -6,6 +6,7 @@ from keras.preprocessing import image
 from PIL import Image
 import numpy as np
 import cv2
+import tensorflow as tf
 
 import threading, time, datetime
 import sys
@@ -16,6 +17,7 @@ model = MobileNet(weights='imagenet')
 
 # 対象物があるかどうかを示すフラグ
 not_exist = True
+graph = tf.get_default_graph()
 
 class CameraThread(threading.Thread):
     def __init__(self):
@@ -26,46 +28,48 @@ class CameraThread(threading.Thread):
         global not_exist
         global camera
         global rawCapture
+        global graph
 
+        with graph.as_default():
         # allow the camera to warmup
-        time.sleep(0.1)
+            time.sleep(0.1)
 
-        # capture frames from camera
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image, then initialize the timestamp
-            # and occupied/unoccupied text
-            print('camera is moving')
-            image = frame.array
-            # machine learning
-            # resize to mobile size(224, 224)
-            img = Image.fromarray(np.uint8(image))
-            img = img.resize((224, 224))
-            x = img
-            pred_data = np.expand_dims(x, axis=0)
-            print(preprocess_input(pred_data))
-            preds = model.predict(preprocess_input(pred_data))
-            results = decode_predictions(preds, top=1)[0]
-            for result in results:
-                # print(result)
-                label = result[1]
-                accu = str(result[2])
-            # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # capture frames from camera
+            for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+                # grab the raw NumPy array representing the image, then initialize the timestamp
+                # and occupied/unoccupied text
+                print('camera is moving')
+                image = frame.array
+                # machine learning
+                # resize to mobile size(224, 224)
+                img = Image.fromarray(np.uint8(image))
+                img = img.resize((224, 224))
+                x = img
+                pred_data = np.expand_dims(x, axis=0)
+                print(preprocess_input(pred_data))
+                preds = model.predict(preprocess_input(pred_data))
+                results = decode_predictions(preds, top=1)[0]
+                for result in results:
+                    # print(result)
+                    label = result[1]
+                    accu = str(result[2])
+                # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-            # show the frame
-            new_label = label + accu
-            image = cv2.putText(image, new_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                # show the frame
+                new_label = label + accu
+                image = cv2.putText(image, new_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-            cv2.imshow("Frame", image)
-            key = cv2.waitKey(1) & 0xFF
-            # 止まるように
-            if result[1] == stop_item:
-                    not_exist = False
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
+                cv2.imshow("Frame", image)
+                key = cv2.waitKey(1) & 0xFF
+                # 止まるように
+                if result[1] == stop_item:
+                        not_exist = False
+                # clear the stream in preparation for the next frame
+                rawCapture.truncate(0)
 
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
+                # if the `q` key was pressed, break from the loop
+                if key == ord("q"):
+                    break
 
 
 class MorterThread(threading.Thread):
