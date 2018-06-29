511b8607c1de6a8a4ad555cbf17098bfa9bd3314
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-6.local>
2018-06-29 14:16:03 +0900

    add motor and print

---
+++ code0629/motor_and_print.py
@@ -0,0 +1,45 @@
+import wiringpi
+
+import threading, time, datetime
+import sys
+import os
+
+def motor():
+    if order == "go":
+        wiringpi.digitalWrite(motor1_pin, 1)
+        wiringpi.digitalWrite(motor2_pin, 0)
+        time.sleep(10)
+        wiringpi.digitalWrite(motor1_pin, 1)
+        wiringpi.digitalWrite(motor2_pin, 1)
+
+    elif order == "back":
+        wiringpi.digitalWrite(motor1_pin, 0)
+        wiringpi.digitalWrite(motor2_pin, 1)
+        time.sleep(10)
+        wiringpi.digitalWrite(motor1_pin, 1)
+        wiringpi.digitalWrite(motor2_pin, 1)
+
+
+def loop_print():
+    count = 0
+    while True:
+        print(count)
+        count += 1
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    param = sys.argv
+    order = param[1]
+    motor1_pin = 23
+    motor2_pin = 24
+
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(motor1_pin, 1)
+    wiringpi.pinMode(motor2_pin, 1)
+
+    th1 = threading.Thread(target=motor, name="motor", args=())
+    th2 = threading.Thread(target=loop_print, name="loop_print", args=())
+
+    th1.start()
+    th2.start()
